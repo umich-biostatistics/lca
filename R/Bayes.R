478caec4a33b5d7f70b6627b63eacf97714a8eca
Kleinsasser <mkleinsa@UMICH.EDU>
2019-11-08 11:35:09 -0500

    Bayes method API

--- R/Bayes.R
+++ R/Bayes.R
@@ -1,1 +1,14 @@
 
+#' Bayesian Latent Class Analysis
+#' 
+#' Perform Bayesian LCA
+#'
+#' Given a set of categorical predictors, draw posterior distribution of 
+#' probability of class membership for each observation.
+#' 
+#' 
+#'
+
+Bayes = function(formula, data, init, ...) {
+  
+}
