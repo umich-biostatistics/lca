bf7a378a3d69dc6ad9181e271aa036e04a503060
Kleinsasser <mkleinsa@UMICH.EDU>
2019-11-12 11:16:16 -0500

    generalizing method for users

--- R/Bayes.R
+++ R/Bayes.R
@@ -9,13 +9,22 @@
 #' @param formula
 #' @param data
 #' @param nclasses
-#' @param init
+#' @param manifest character vector containing the names of each manifest variable,
+#' e.g., manifest = c("Z1", "med_3", "X5")
+#' @param inits list of initial values for WinBUGS. Defaults will be set if nothing
+#' is specified.
+#' @param return.bugs = FALSE. If TRUE, return the bugs model as a character string.
+#' This can be further edited to suit the researchers needs, e.g., by adding hyper-prior
+#' distributions.
+#' @param dir Specify full path to the directory where you want
+#' to store the WinBUGS output files and BUGS model file.
 #' @param ...
 #' 
 #' @return a named list of draws.
 #'
 
-bayes_lca = function(formula, data, nclasses, init, manifest, ...) {
+bayes_lca = function(formula, data, nclasses, manifest, inits, return.bugs, dir, 
+                     n.chains, n.iter, parameters.to.save, ...) {
   
   # checks on input
   
@@ -31,6 +40,9 @@ bayes_lca = function(formula, data, nclasses, init, manifest, ...) {
     stop("A data.frame is required to fit the model.")
   }
   
+  N = nrow(data)
+  n_manifest = length(manifest)
+  
   # construct a model frame (mf)
   
   mf = match.call(expand.dots = FALSE)
@@ -44,12 +56,43 @@ bayes_lca = function(formula, data, nclasses, init, manifest, ...) {
   mt = attr(mf, "terms")
   x = model.matrix(mt, mf)
   
+  # select manifest variables from model matrix
+  if(any(!(manifest %in% colnames(mf)))) {
+    stop("At least one manifest variable name is not in the names of variables
+         in the model frame.")
+  }
+  
+  Z = mf[,manifest]
+  
+  pclass_prior = round(rep(1/nclasses, nclasses), digits = 3)
+  if(sum(pclass_prior) != 1){
+    pclass_prior[length(pclass_prior)] = pclass_prior[length(pclass_prior)] + 
+      (1 - sum(pclass_prior))
+  }
+  
+  dat_list = list(
+    Z = structure(
+      .Data=as.vector(Z),
+      .Dim=c(N,n_manifest)
+    ),
+    prior = pclass_prior,
+    prior4 = c(0.25, 0.25, 0.25, 0.25)
+  )
+  
   # construct R2WinBUGS input
   
   # call R bugs model constructor
-  model = constr_bugs_model()
+  model = constr_bugs_model(N = N, n_manifest = n_manifest,
+                            nclasses = nclasses)
+  # write model
+  filename <- file.path(dir, "model.bug")
+  write.model(constr_bugs_model(N = N, n_manifest = n_manifest, nclasses = nclasses), filename)
   
   # Fit Bayesian latent class model
+  samp_lca = bugs(data = dat_list, inits = inits,
+                  model.file = filename, n.chains = n.chains, 
+                  n.iter = n.iter, parameters.to.save = parameters.to.save, 
+                  debug = TRUE)
   
   # Results
   # return bugs fit
@@ -81,7 +124,6 @@ constr_bugs_model = function(N, n_manifest, nclasses) {
         bugs_model_func = function() {
           
           for (i in 1:!!N){
-            
             true[i]~dcat(theta[])
             
             for(j in 1:!!n_manifest){
@@ -106,7 +148,6 @@ constr_bugs_model = function(N, n_manifest, nclasses) {
  
   text_fun = as.character(quo_get_expr(constructor()))[2]
   return(eval(parse(text = text_fun)))
-
   
 }
 
--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This `R` package implements expectation maximization (EM) and Bayesian latent class analyses (insert reference).
 
+# Dependencies
+
+This package uses the R interface for WinBUGS called `R2WinBUGS`. The package `R2WinBUGS` in turn depends on the standalone Windows program `WinBUGS`. Follow this link: [University of Cambridge MRC Biostatistics Unit](https://www.mrc-bsu.cam.ac.uk/software/bugs/the-bugs-project-winbugs/) for download and installation instructions. `WinBUGS` can also be used on Mac using [Wine](https://www.winehq.org/). Plenty of tutorials that demonstrate install of WinBUGS on non-Windows machines are available on Google. 
+
+Once the standalone `WinBUGS` program is installed, open R and run:
+
 ```r
 library(devtools)
 # may take some time:
@@ -13,6 +19,15 @@ library(lca)
 ### Example
 
 Example code
+
+The main function for Bayesian LCA is `bayes_lca()`. Use `?bayes_lca()` for the R
+help file.
+
+Here is an example Bayesian latent class analysis on simulated data:
+
+```r
+
+```
 
 ### Vignette
 
